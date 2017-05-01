ea9ed80f73ff05218b7125f409db0bd746d87c99
hongchh <hongchh_sysu@qq.com>
2017-05-01 13:28:14 +0800

    完善Model/Store/Helper

---
+++ UWPApp/UWPApp/Model/Record.cs
@@ -0,0 +1,30 @@
+﻿namespace UWPApp.Model
+{
+    public class Record
+    {
+        public Record (string id = "", string userId = "", string title = "",
+            string content = "", string image = "", string audio = "",
+            string video = "", long date = 0, int favoriteNum = 0)
+        {
+            this.id = id;
+            this.userId = userId;
+            this.title = title;
+            this.content = content;
+            this.image = image;
+            this.audio = audio;
+            this.video = video;
+            this.date = date;
+            this.favoriteNum = favoriteNum;
+        }
+
+        public string id;
+        public string userId;
+        public string title;
+        public string content;
+        public string image;
+        public string audio;
+        public string video;
+        public long date;
+        public int favoriteNum;
+    }
+}
---
+++ UWPApp/UWPApp/Store/RecordStore.cs
@@ -0,0 +1,22 @@
+﻿namespace UWPApp.Store
+{
+    public class RecordStore
+    {
+        private static RecordStore instance = null;
+        private static readonly object padlock = new object();
+
+        private RecordStore () { }
+
+        public static RecordStore getInstance ()
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new RecordStore();
+                }
+                return instance;
+            }
+        }
+    }
+}
