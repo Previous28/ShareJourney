910dc903b5e4e3df3f0865f8d24f26fc6e75da96
hongchh <hongchh_sysu@qq.com>
2017-05-04 09:27:56 +0800

    完成服务端的点赞逻辑

--- Server/api/record.js
+++ Server/api/record.js
@@ -65,11 +65,11 @@ api.get('/all', (req, res) => {
 api.get('/detail', (req, res) => {
   Record.findById(req.params.recordId).then(record => {
     record.favoriter = []
-    Favorite.find({ _id: new ObjectId(req.params.recordId) }).then(records => {
-      let count = records.length;
+    Favorite.find({ _id: new ObjectId(req.params.recordId) }).then(favorites => {
+      let count = favorites.length;
       count ? '' : res.json({ result: 'ok', record })
-      for (let i = 0; i < records.length; ++i) {
-        User.findById(records[i].userId).then(user => {
+      for (let i = 0; i < favorites.length; ++i) {
+        User.findById(favorites[i].userId).then(user => {
           user ? record.favoriter.push(user.avatar) : ''
           count ? --count : res.json({ result: 'ok', record })
         })
@@ -78,9 +78,22 @@ api.get('/detail', (req, res) => {
   })
 })
 
-// TODO: 点赞
+// 点赞
 api.get('/favorite', (req, res) => {
-  res.json({})
+  Online.findById(req.params.onlineId).then(online => {
+    if (!online) {
+      res.json({ result: 'error' })
+    } else {
+      Favorite.findOne({ userId: online.userId }).then(favorite => {
+        if (!favorite) {
+          new Favorite({ userId: online.userId, recordId: req.params.recordId })
+          .save().then(() => { res.json({ result: 'ok' }) })
+        } else {
+          res.json({ result: 'ok' })
+        }
+      })
+    }
+  })
 })
 
 module.exports = api
