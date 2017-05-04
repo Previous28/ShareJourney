6124d3744a37b4ed1d809325860cc541608792b7
hongchh <hongchh_sysu@qq.com>
2017-05-04 15:02:14 +0800

    完成服务端权限模块的调试和改进

--- Server/api/auth.js
+++ Server/api/auth.js
@@ -9,10 +9,10 @@ module.exports = (User, Online) => {
 
   // 登录
   api.post('/signin', (req, res) => {
-    if (req.body.username && req.body.body.password) {
+    if (req.body.username && req.body.password) {
       User.findOne({ username: req.body.username })
       .then(user => {
-        if (md5(req.body.password) === user.password) {
+        if (user && md5(req.body.password) === user.password) {
           Online.findById(user._id).then(online => {
             if (!online) {
               new Online({ userId: user._id }).save().then(online => {
@@ -39,19 +39,27 @@ module.exports = (User, Online) => {
 
   // 注册
   api.post('/signup', (req, res) => {
-    if (req.body.username && req.body.body.password) {
-      new User({
-        username: req.body.username,
-        password: md5(req.body.password),
-        nickname: 'default',
-        avatar: 'default'
-      }).save().then(user => {
-        new Online({ userId: user._id }).save().then(online => {
-          res.json({
-            result: 'ok',
-            onlineId: online._id
+    if (req.body.username && req.body.password &&
+        req.body.username.length > 7 && req.body.password.length > 7 &&
+        req.body.username.length < 16 && req.body.password.length < 16) {
+      User.findOne({ username: req.body.username }).then(user => {
+        if (user) {
+          res.json({ result: 'error' })
+        } else {
+          new User({
+            username: req.body.username,
+            password: md5(req.body.password),
+            nickname: 'default',
+            avatar: 'default'
+          }).save().then(user => {
+            new Online({ userId: user._id }).save().then(online => {
+              res.json({
+                result: 'ok',
+                onlineId: online._id
+              })
+            })
           })
-        })
+        }
       })
     } else {
       res.json({ result: 'error' })
@@ -60,15 +68,9 @@ module.exports = (User, Online) => {
 
   // 退出
   api.get('/signout', (req, res) => {
-    if (req.params.onlineId) {
-      Online.findById(req.params.onlineId)
-      .then(online => {
-        if (!online) {
-          res.json({ result: 'error' })
-        } else {
-          Online.findByIdAndRemove(req.params.onlineId)
-          res.json({ result: 'ok' })
-        }
+    if (req.query.onlineId) {
+      Online.findByIdAndRemove(req.query.onlineId).then(online => {
+        res.json({ result: 'ok' })
       })
     } else {
       res.json({ result: 'error' })
