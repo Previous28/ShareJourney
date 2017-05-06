9757de846d7a46efcb3a9e1a4d692c4623ce0600
hongchh <hongchh_sysu@qq.com>
2017-05-06 16:28:49 +0800

    完成User界面；调整Main界面样式

--- UWPApp/UWPApp/View/UserPage.xaml.cs
+++ UWPApp/UWPApp/View/UserPage.xaml.cs
@@ -13,18 +13,67 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
-
 namespace UWPApp.View
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
     public sealed partial class UserPage : Page
     {
         public UserPage()
         {
             this.InitializeComponent();
+            Data = Store.RecordStore.getInstance();
+        }
+
+        private Store.RecordStore Data;
+
+        // 更新用户信息
+        private bool modify = false;
+        private void setUserInfo(object sender, RoutedEventArgs e)
+        {
+            if (modify)
+            {
+                // TODO: 更新用户信息
+                modify = false;
+                nicknameText.Visibility = Visibility.Visible;
+                nickname.Visibility = Visibility.Collapsed;
+                password.Visibility = Visibility.Collapsed;
+                confirm.Visibility = Visibility.Collapsed;
+                actionBtn.Content = "Modify";
+            }
+            else
+            {
+                modify = true;
+                nicknameText.Visibility = Visibility.Collapsed;
+                nickname.Visibility = Visibility.Visible;
+                password.Visibility = Visibility.Visible;
+                confirm.Visibility = Visibility.Visible;
+                actionBtn.Content = "Update";
+            }
+        }
+
+        // 页面跳转
+        private void goToOtherPage(object sender, RoutedEventArgs e)
+        {
+            AppBarButton btn = sender as AppBarButton;
+            if (btn.Name == "main")
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(MainPage));
+            }
+            else if (btn.Name == "user")
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(UserPage));
+            }
+            else if (btn.Name == "edit")
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(EditPage));
+            }
+            else if (btn.Name == "detail")
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(DetailPage));
+            }
+            else
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(AuthPage));
+            }
         }
     }
 }
