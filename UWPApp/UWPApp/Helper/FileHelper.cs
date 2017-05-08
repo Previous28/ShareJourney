e354dc83cb0ce5863776fccf78aa929fd026dabc
hongchh <hongchh_sysu@qq.com>
2017-05-08 23:16:54 +0800

    完善FileHelper选择音频和视频文件的功能

--- UWPApp/UWPApp/Helper/FileHelper.cs
+++ UWPApp/UWPApp/Helper/FileHelper.cs
@@ -7,6 +7,7 @@ namespace UWPApp.Helper
 {
     class FileHelper
     {
+        // 选择图片
         public static async Task<StorageFile> selectImage()
         {
             // 文件选择器，默认打开目录为图片目录，// 过滤图片文件
@@ -16,6 +17,29 @@ namespace UWPApp.Helper
             filePicker.FileTypeFilter.Add(".jpeg");
             filePicker.FileTypeFilter.Add(".png");
             filePicker.FileTypeFilter.Add(".gif");
+            // 选择一个文件
+            return await filePicker.PickSingleFileAsync();
+        }
+
+        // 选择音频
+        public static async Task<StorageFile> selectAudio()
+        {
+            // 文件选择器，默认打开目录为音乐目录，// 过滤音频文件
+            var filePicker = new FileOpenPicker();
+            filePicker.SuggestedStartLocation = PickerLocationId.MusicLibrary;
+            filePicker.FileTypeFilter.Add(".mp3");
+            // 选择一个文件
+            return await filePicker.PickSingleFileAsync();
+        }
+
+        // 选择视频
+        public static async Task<StorageFile> selectVideo()
+        {
+            // 文件选择器，默认打开目录为视频目录，// 过滤视频文件
+            var filePicker = new FileOpenPicker();
+            filePicker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            filePicker.FileTypeFilter.Add(".mp4");
+            filePicker.FileTypeFilter.Add(".avi");
             // 选择一个文件
             return await filePicker.PickSingleFileAsync();
         }
