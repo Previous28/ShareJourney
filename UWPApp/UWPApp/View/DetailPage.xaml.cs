a8f9679e0d0feff49d528e365010adc9dd3a954b
Previous28 <shujh_sysu@163.com>
2017-05-10 23:00:18 +0800

    实现了详情界面部分逻辑，在详情界面增加了标题一栏。修改了编辑界面的按钮样式

--- UWPApp/UWPApp/View/DetailPage.xaml.cs
+++ UWPApp/UWPApp/View/DetailPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
@@ -24,12 +26,13 @@ namespace UWPApp.View
     /// </summary>
     public sealed partial class DetailPage : Page
     {
+        private Model.Record currentRecord;
         public DetailPage()
         {
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
@@ -44,14 +47,78 @@ namespace UWPApp.View
                 SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
                     AppViewBackButtonVisibility.Collapsed;
             }
+
+            currentRecord = ((Model.Record)e.Parameter);
+            //await (new MessageDialog(currentRecord.image + currentRecord.audio + currentRecord.video)).ShowAsync();
+
+            //同步顶栏信息
+            avatarInTopBar.ImageSource = currentRecord.userAvatar;
+            publisher.Text = currentRecord.nickname;
+            publishTime.Text = currentRecord.date;
+
+            //同步宽屏标题、文字信息
+            title1.Text = currentRecord.title;
+            content1.Text = currentRecord.content;
+            //同步窄屏标题、文字信息
+            title2.Text = currentRecord.title;
+            content2.Text = currentRecord.content;
+
+            //判断是否显示图片、音频、视频
+            if (currentRecord.image != "")
+            {
+                image1.Visibility = Visibility.Visible;//宽屏
+                image2.Visibility = Visibility.Visible;//窄屏
+
+                BitmapImage newSource = null;
+                string _newSource = Helper.NetworkHelper.SERVER + currentRecord.image;
+                newSource = new BitmapImage(new Uri(_newSource));
+                image1.Source = image2.Source = newSource;
+            }
+            else if (currentRecord.audio != "")
+            {
+                //宽屏
+                musicBg1.Visibility = Visibility.Visible;
+                timelineSlider1.Visibility = Visibility.Visible;
+                play1.Visibility = Visibility.Visible;
+                //窄屏
+                musicBg2.Visibility = Visibility.Visible;
+                timelineSlider2.Visibility = Visibility.Visible;
+                play2.Visibility = Visibility.Visible;
+                string newSource = Helper.NetworkHelper.SERVER + currentRecord.audio;
+                mediaElement1.Source = mediaElement2.Source = new Uri(newSource);
+            }
+            else if (currentRecord.video != "")
+            {
+                //宽屏
+                mediaElement1.Visibility = Visibility.Visible;
+                timelineSlider1.Visibility = Visibility.Visible;
+                play1.Visibility = Visibility.Visible;
+                //窄屏
+                mediaElement2.Visibility = Visibility.Visible;
+                timelineSlider2.Visibility = Visibility.Visible;
+                play2.Visibility = Visibility.Visible;
+                string newSource = Helper.NetworkHelper.SERVER + currentRecord.video;
+                mediaElement1.Source = mediaElement2.Source = new Uri(newSource);
+            }
+            //同步点赞数
+            favoriteNum.Text = currentRecord.favoriteNum.ToString();
+            JObject res = await Helper.NetworkHelper.recordDetail(currentRecord.id);
+            if (res["result"].ToString() == Helper.NetworkHelper.SUCCESS)
+            {
+                JArray records = JArray.Parse(res["records"].ToString());
+                
+            }
         }
 
         private void SeekMediaPosition(object sender, RangeBaseValueChangedEventArgs e)
         {
-            int SliderValue = (int)timelineSlider.Value;
+            int SliderValue1 = (int)timelineSlider1.Value;
+            int SliderValue2 = (int)timelineSlider2.Value;
 
-            TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue);
-            mediaElement.Position = ts;
+            TimeSpan ts1 = new TimeSpan(0, 0, 0, 0, SliderValue1);
+            mediaElement1.Position = ts1;
+            TimeSpan ts2 = new TimeSpan(0, 0, 0, 0, SliderValue2);
+            mediaElement2.Position = ts2;
         }
 
         private void playClick(object sender, RoutedEventArgs e)
