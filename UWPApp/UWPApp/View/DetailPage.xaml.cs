adda3573dde618862d771ee6fa0c4a6eb606a8dd
Previous28 <shujh_sysu@163.com>
2017-05-11 20:22:01 +0800

    完善了详情界面的逻辑，完善了编辑界面的逻辑，发表带音频或视频的记录可能还有bug，待测试。

--- UWPApp/UWPApp/View/DetailPage.xaml.cs
+++ UWPApp/UWPApp/View/DetailPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
@@ -16,6 +17,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
@@ -27,6 +29,8 @@ namespace UWPApp.View
     public sealed partial class DetailPage : Page
     {
         private Model.Record currentRecord;
+        GridView gridView = new GridView();
+
         public DetailPage()
         {
             this.InitializeComponent();
@@ -54,7 +58,6 @@ namespace UWPApp.View
             //同步顶栏信息
             avatarInTopBar.ImageSource = currentRecord.userAvatar;
             publisher.Text = currentRecord.nickname;
-            publishTime.Text = currentRecord.date;
 
             //同步宽屏标题、文字信息
             title1.Text = currentRecord.title;
@@ -102,12 +105,47 @@ namespace UWPApp.View
             }
             //同步点赞数
             favoriteNum.Text = currentRecord.favoriteNum.ToString();
+            //解析点赞头像组
             JObject res = await Helper.NetworkHelper.recordDetail(currentRecord.id);
             if (res["result"].ToString() == Helper.NetworkHelper.SUCCESS)
             {
-                JArray records = JArray.Parse(res["records"].ToString());
+                JObject records = JObject.Parse(res["record"].ToString());
+                JArray favoriters = JArray.Parse(records["favoriter"].ToString());
                 
+                gridView.IsItemClickEnabled = false;
+                gridView.Name = "gridView";
+                gridView.Margin = new Thickness(30, 0, 0, 0);
+                for (int i = 0; i < favoriters.Count; ++i)
+                {
+                    string _newFavoriter = Helper.NetworkHelper.SERVER + favoriters[i].ToString();
+                    Ellipse ellipse = new Ellipse();
+                    ImageBrush newFavoriter = new ImageBrush();
+                    newFavoriter.ImageSource = new BitmapImage(new Uri(_newFavoriter));
+                    ellipse.Fill = newFavoriter;
+                    ellipse.Height = ellipse.Width = 60;
+                    gridView.Items.Add(ellipse);
+                }
+                // Add the grid view to a parent container in the visual tree.
+                likeList.Children.Add(gridView);
+                //获取更详细的发布时间
+                publishTime.Text = records["date"].ToString();
             }
+        }
+
+        //定义媒体播放器的一些逻辑
+        private void ElementMediaOpened(object sender, RoutedEventArgs e)
+        {
+            var ts = mediaElement1.NaturalDuration.TimeSpan;
+            timelineSlider1.Maximum = ts.TotalMilliseconds;
+            timelineSlider2.Maximum = ts.TotalMilliseconds;
+        }
+
+        private void ElementMediaEnded(object sender, RoutedEventArgs e)
+        {
+            if (mediaElement1.Visibility == Visibility.Visible)
+                mediaElement1.Stop();
+            else
+                mediaElement2.Stop();
         }
 
         private void SeekMediaPosition(object sender, RangeBaseValueChangedEventArgs e)
@@ -123,12 +161,79 @@ namespace UWPApp.View
 
         private void playClick(object sender, RoutedEventArgs e)
         {
-
+            if (mediaElement1.Visibility == Visibility.Visible)
+            {
+                mediaElement1.Play();
+                play1.Visibility = Visibility.Collapsed;
+                stop1.Visibility = Visibility.Visible;
+            } else
+            {
+                mediaElement2.Play();
+                play2.Visibility = Visibility.Collapsed;
+                stop2.Visibility = Visibility.Visible;
+            }
         }
 
         private void stopClick(object sender, RoutedEventArgs e)
         {
+            if (mediaElement1.Visibility == Visibility.Visible)
+            {
+                mediaElement1.Stop();
+                play1.Visibility = Visibility.Visible;
+                stop1.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                mediaElement2.Stop();
+                play2.Visibility = Visibility.Visible;
+                stop2.Visibility = Visibility.Collapsed;
+            }
+        }
 
+        private void myMediaElementLoaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += (ss, ee) =>
+            {
+                //显示当前视频进度
+                if (mediaElement1.Visibility == Visibility.Visible)
+                {
+                    var ts = mediaElement1.Position;
+                    timelineSlider1.Value = ts.TotalMilliseconds;
+                }
+                else
+                {
+                    var ts = mediaElement2.Position;
+                    timelineSlider2.Value = ts.TotalMilliseconds;
+                }
+            };
+            timer.Start();
+        }
+
+        //实现点赞功能
+        private async void favorite(object sender, RoutedEventArgs e)
+        {
+            JObject res = await Helper.NetworkHelper.favorite(Store.UserStore.onlineId, currentRecord.id);
+            if (res["result"].ToString() == Helper.NetworkHelper.SUCCESS)
+            {
+                //Store.RecordStore.loadAllRecordsFromServer();
+                //Store.RecordStore.loadUserRecordsFromServer();
+
+                long tmp = (currentRecord.favoriteNum) + 1;
+                favoriteNum.Text = tmp.ToString();
+                //在下方点赞列表显示头像
+                Ellipse ellipse = new Ellipse();
+                ImageBrush newFavoriter = new ImageBrush();
+                newFavoriter.ImageSource = Store.UserStore.avatar;
+                ellipse.Fill = newFavoriter;
+                ellipse.Height = ellipse.Width = 60;
+                gridView.Items.Add(ellipse);
+            }
+            else
+            {
+                await(new MessageDialog("您已经点过赞了！")).ShowAsync();
+            }
         }
     }
 
--- UWPApp/UWPApp/View/EditPage.xaml.cs
+++ UWPApp/UWPApp/View/EditPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
@@ -12,6 +15,11 @@ namespace UWPApp.View
 {
     public sealed partial class EditPage : Page
     {
+        private StorageFile file;
+        private bool isImage = false;
+        private bool isAudio = false;
+        private bool isVideo = false;
+
         public EditPage()
         {
             this.InitializeComponent();
@@ -36,29 +44,154 @@ namespace UWPApp.View
             }
         }
 
-        private void pickPicture(object sender, RoutedEventArgs e)
+        private async void pickPicture(object sender, RoutedEventArgs e)
         {
-
+            file = await Helper.FileHelper.selectImage();
+            if (file != null)
+            {
+                if (isAudio)
+                {
+                    isAudio = false;
+                    audio.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage2.png"));
+                }
+                else if (isVideo)
+                {
+                    isVideo = false;
+                    video.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage3.png"));
+                }
+                if (!isImage)
+                {
+                    isImage = true;
+                    string _newSource = "ms-appx:///Assets/imagePicked.png";
+                    BitmapImage newSource = new BitmapImage(new Uri(_newSource));
+                    image.Source = newSource;
+                }
+            }
         }
 
-        private void pickMusic(object sender, RoutedEventArgs e)
+        private async void pickMusic(object sender, RoutedEventArgs e)
         {
-
+            file = await Helper.FileHelper.selectAudio();
+            if (file != null)
+            {
+                if (isImage)
+                {
+                    isImage = false;
+                    image.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage1.png"));
+                }
+                else if (isVideo)
+                {
+                    isVideo = false;
+                    video.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage3.png"));
+                }
+                if (!isAudio)
+                {
+                    isAudio = true;
+                    string _newSource = "ms-appx:///Assets/audioPicked.png";
+                    BitmapImage newSource = new BitmapImage(new Uri(_newSource));
+                    audio.Source = newSource;
+                }
+            }
         }
 
-        private void pickVideo(object sender, RoutedEventArgs e)
+        private async void pickVideo(object sender, RoutedEventArgs e)
         {
-
+            file = await Helper.FileHelper.selectVideo();
+            if (file != null)
+            {
+                if (isImage)
+                {
+                    isImage = false;
+                    image.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage1.png"));
+                }
+                else if (isAudio)
+                {
+                    isAudio = false;
+                    audio.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage2.png"));
+                }
+                if (!isVideo)
+                {
+                    isVideo = true;
+                    string _newSource = "ms-appx:///Assets/videoPicked.png";
+                    BitmapImage newSource = new BitmapImage(new Uri(_newSource));
+                    video.Source = newSource;
+                }
+            }
         }
 
         private void cancelClick(object sender, RoutedEventArgs e)
         {
-
+            title.Text = content.Text = "";
+            isImage = isAudio = isVideo = false;
+            image.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage1.png"));
+            audio.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage2.png"));
+            video.Source = new BitmapImage(new Uri("ms-appx:///Assets/imageForEditPage3.png"));
         }
 
-        private void publishClick(object sender, RoutedEventArgs e)
+        private async void publishClick(object sender, RoutedEventArgs e)
         {
-
+            JObject res1 = await Helper.NetworkHelper.postRecord(title.Text, content.Text, Store.UserStore.onlineId);
+            if (res1["result"].ToString() == Helper.NetworkHelper.SUCCESS)
+            {
+                JObject records = JObject.Parse(res1["record"].ToString());
+                JObject res2 = null;
+                if (isImage)
+                {
+                    res2 = await Helper.NetworkHelper.uploadImage(file, Store.UserStore.onlineId, records["_id"].ToString());
+                }
+                else if (isAudio)
+                {
+                    res2 = await Helper.NetworkHelper.uploadAudio(file, Store.UserStore.onlineId, records["_id"].ToString());
+                }
+                else if (isVideo)
+                {
+                    res2 = await Helper.NetworkHelper.uploadVideo(file, Store.UserStore.onlineId, records["_id"].ToString());
+                }
+                if (res2 != null)
+                {
+                    if (res2["result"].ToString() == Helper.NetworkHelper.SUCCESS)
+                    {
+                        await (new MessageDialog("发布成功！")).ShowAsync();
+                        Model.Record newRecord1 = new Model.Record();
+                        newRecord1.id = records["_id"].ToString();
+                        newRecord1.userId = Store.UserStore.userId;
+                        newRecord1.title = title.Text;
+                        newRecord1.content = content.Text;
+                        newRecord1.date = System.DateTime.Now.ToString();
+                        newRecord1.nickname = Store.UserStore.nickname;
+                        newRecord1.favoriteNum = 0;
+                        newRecord1.userAvatar = Store.UserStore.avatar;
+                        if (isImage) newRecord1.image = res2["path"].ToString();
+                        else if (isAudio) newRecord1.audio = res2["path"].ToString();
+                        else if (isVideo) newRecord1.video = res2["path"].ToString();
+                        (Window.Current.Content as Frame).Navigate(typeof(DetailPage), newRecord1);
+                    }
+                    else
+                    {
+                        await (new MessageDialog("上传失败！")).ShowAsync();
+                    }
+                }
+                
+                if (res2 == null)
+                {
+                    await (new MessageDialog("发布成功！")).ShowAsync();
+                    Model.Record newRecord = new Model.Record();
+                    newRecord.id = records["_id"].ToString();
+                    newRecord.userId = Store.UserStore.userId;
+                    newRecord.title = title.Text;
+                    newRecord.content = content.Text;
+                    newRecord.image = newRecord.video = newRecord.audio = "";
+                    newRecord.date = System.DateTime.Now.ToString();
+                    newRecord.nickname = Store.UserStore.nickname;
+                    newRecord.favoriteNum = 0;
+                    newRecord.userAvatar = Store.UserStore.avatar;
+                    (Window.Current.Content as Frame).Navigate(typeof(DetailPage), newRecord);
+                }
+            }
+            else
+            {
+                await (new MessageDialog("发布失败！")).ShowAsync();
+            }
         }
     }
 }
